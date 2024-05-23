d9da64a15bb5e26874d7b581a5edb84a156e903c
unknown <jophadalecarlvelasco94@gmail.com>
2024-05-23 16:53:34 +0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Text, BackHandler } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/Home";
+import FireExstinguisher from "./screens/fire_exstinguisher";
+import FireExit from "./screens/fire_exit";
+import FirstAid from "./screens/first_aid";
+import PathFinder from "./screens/path";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createNativeStackNavigator();
+
+function App() {
+	const navigationRef = React.useRef(null);
+
+	return (
+		<NavigationContainer ref={navigationRef}>
+			<Stack.Navigator
+				initialRouteName="Home"
+				screenOptions={{ headerShown: false }}
+			>
+				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="FireExstinguisher" component={FireExstinguisher} />
+				<Stack.Screen name="FirstAid" component={FirstAid} />
+				<Stack.Screen name="FireExit" component={FireExit} />
+				<Stack.Screen name="PathFinder" component={PathFinder} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
